56e75638f46ece664ef9b003285083b60b3c7d92
Ryaee-Www <gu.798@buckeyemail.osu.edu>
2022-01-18 19:50:30 -0500

    Initial establishment of server1.c

--- client1.c
+++ client1.c
@@ -1,3 +1,6 @@
+/*
+ * This program is a data-gram socket client file. It is based on the simple DGRAM socket client illustrated by Professor Dave Ogle, and further implemented and modifed by YuChen Gu on 1/18/2022.
+ */
 #include <string.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -5,7 +8,31 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
+#include <ctype.h>
+int Send(char *buffer, int newSocket, struct sockaddr_in serverAddress);
+
 int main(int argc, char *argv[])
 {
+    int newSocket;
+    struct sockaddr_in serverAddress;
+    int portNum;
+    char serverIP[29];
+    char buffer[100];
+    if (argc < 3){
+        printf("usage is: client <remote IP Address> <remote port number>");
+        exit(1);
+    }
 
+    newSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    portNum = atol(argv[2],NULL,10);
+    strcpy(serverIP,argv[1]);
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(portNum);
+    serverAddress.sin_addr.s_addr = inet_addr(serverIP);
+
+    memset (buffer,'\0',100)
+    sprintf(buffer, )
 }
--- server1.c
+++ server1.c
@@ -1,14 +1,53 @@
+/*
+ * This program is a data-gram socket server file. It is based on the simple DGRAM socket server illustrated by Professor Dave Ogle, and further implemented and modifed by YuChen Gu on 1/18/2022.
+ */
+
 #include <string.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
+#include <arpa/inet.h>
 #include <unistd.h> 
 #include <stdlib.h> 
  
  
 int main(int argc, char *argv[]) { 
-    int socketinfo;
-    int returncode;
-    
-} 
+    int newSocket;
+    int returnCode;
+    struct sockaddr_in serverAddress;
+    struct sockaddr_in clientAddress;
+    char buffer[100];
+    int flag = 0;
+    int portNum;
+    socklen_t clientLength = sizeof(struct sockaddr);
+
+    if (argc < 2){
+        printf("usage is: server <portNumber>\n");
+        exit(1);
+    }
+
+    portNum = atoi(argv[1]);
+
+    newSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(portNum);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    returnCode = bind(newSocket,buffer, (struct sockaddr *) & serverAddress,sizeof(serverAddress));
+
+    if (rc<0){
+        perror ("bind");
+        exit(1)
+    }
+    for(;;;){
+        memset(buffer,'\0',100);
+        returnCode = recvfrom(newSocket,buffer,sizeof(buffer),flag,(struct sockaddr *) &clientAddress, &clientLength ));
+        if(rc<=0){
+            printf("error on receive, quitting...");
+            break;
+        }
+        printf("Reveived %d bytes\n",returnCode);
+    }
+    return 0;
+}
  
